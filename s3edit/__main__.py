f125a102731c454f3749f83fefadbbb863c52d2e
Ben Gyori <ben.gyori@gmail.com>
2019-03-30 18:32:36 -0400

    Basic load/save from S3 implemented

--- s3edit/__main__.py
+++ s3edit/__main__.py
@@ -1,7 +1,7 @@
 import sys
-from .core import edit_file
+from .core import edit_from_s3
 
 
 if __name__ == '__main__':
     path = sys.argv[1]
-    edit_file(path)
+    edit_from_s3(path)
--- s3edit/core.py
+++ s3edit/core.py
@@ -1,16 +1,12 @@
 import os
 import boto3
 import logging
+import tempfile
 import subprocess
 
 
 logger = logging.getLogger(__name__)
-
-
-def edit_file(path):
-    editor = get_editor()
-    logger.info(f'Editing with {editor}: {path}')
-    subprocess.call([editor, path])
+client = boto3.client('s3')
 
 
 def get_editor():
@@ -18,3 +14,43 @@ def get_editor():
     if not editor:
         editor = 'vi'
     return editor
+
+
+def load_from_s3(bucket, key):
+    obj = client.get_object(Bucket=bucket, Key=key)
+    content = obj['Body'].read().decode('utf8')
+    return content
+
+
+def save_to_s3(bucket, key, content):
+    obj = content.encode('utf-8')
+    client.put_object(Body=obj, Bucket=bucket, Key=key)
+
+
+def bucket_key_from_path(path):
+    parts = path.split('/')
+    bucket = parts[0]
+    key = '/'.join(parts[1:])
+    return bucket, key
+
+
+def edit_from_s3(path):
+    bucket, key = bucket_key_from_path(path)
+    content = load_from_s3(bucket, key)
+    content = edit_content(content)
+    save_to_s3(bucket, key, content)
+
+
+def edit_content(content):
+    editor = get_editor()
+    name = None
+    with tempfile.NamedTemporaryFile(suffix='.tmp', mode='w',
+                                     delete=False) as tf:
+        name = tf.name
+        tf.write(content)
+        tf.flush()
+        subprocess.call([editor, name])
+        tf.close()
+    with open(name, 'r') as fh:
+        content = fh.read()
+    return content
